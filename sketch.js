1b5cf2ad6f18411b494c0958d1ccb692efc5b251
Beatriz Helena de Mello Orlandi <94700968+BeatrizOrlandi@users.noreply.github.com>
2023-01-26 20:00:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,9 +7,9 @@ var cars = [];
 
 function preload() {
   backgroundImage = loadImage("./assets/planodefundo.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/PISTA.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/PISTA.png");
 }
 
 function setup() {
